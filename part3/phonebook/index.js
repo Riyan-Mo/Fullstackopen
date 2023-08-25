1e8830f509f825e4b92e3acae9d66866bfa56f91
Riyan-Mo <Riyan8308151899@gmail.com>
2023-08-25 13:32:57 +0530

    Added frontend to build

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
+app.use(express.static('dist'));
+
 app.use(morgan(function (tokens, req, res) {
   return [
     tokens.method(req, res),
@@ -38,10 +40,6 @@ let phonebook = [
 ]
 
 app.get('/api/persons', (request, response)=>{
-  response.json(phonebook);
-})
-
-app.get('/', (request, response)=>{
   response.json(phonebook);
 })
 
