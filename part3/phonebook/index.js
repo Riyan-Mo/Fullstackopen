8314225bd8c9fdc863438b87775fdf3fcc04814c
Riyan-Mo <Riyan8308151899@gmail.com>
2023-08-29 08:51:17 +0530

    Exercise 3.18 done

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,7 @@
+const Phonebook = require('./models/entry')
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.json());
@@ -16,31 +18,16 @@ app.use(morgan(function (tokens, req, res) {
   ].join(' ')
 }))
 
-let phonebook = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+app.get('/api/persons/:id', (request, response)=>{
+  Phonebook.findById(request.params.id).then(result=>{
+    response.json(result);
+  })
+})
 
-app.get('/api/persons', (request, response)=>{
-  response.json(phonebook);
+app.get('/api/persons/', (request, response)=>{
+  Phonebook.find({}).then(result=>{
+    response.json(result);
+  })
 })
 
 app.get('/info', (request, response)=>{
@@ -58,7 +45,6 @@ const generateId = () =>{
 
 app.post('/api/persons/', (request, response)=>{
   const body = request.body;
-  const alreadyExists = phonebook.filter(entry=>entry.name===body.name).length!==0
   if(!body){
     return response.status(404).json({
       error:'content missing'
@@ -74,21 +60,50 @@ app.post('/api/persons/', (request, response)=>{
       error: 'name already exists in records',
     })
   }
-  const newPerson = {
-    content: body.content,
-    important: body.important||false,
-    id:generateId()
-  }
-  phonebook.concat(newPerson);
+  const newPerson = new Phonebook({
+    name:body.name,
+    number:body.number
+  })
+  newPerson.save().then(result=>{
+    console.log(newPerson)
+    mongoose.connection.close();
+  })
   response.json(newPerson);
 })
 
-app.delete('/api/persons/:id', (request, response)=>{
-  const id = Number(request.params.id);
-  phonebook = phonebook.filter(person=>person.id!==id);
-  console.log(phonebook);
-  response.status(204).end();
+app.put('/api/persons/:id', (request, response, next)=>{
+  const body = request.body
+
+  const entry = new Phonebook({
+    name: body.name,
+    number: body.number,
+    id: request.params.id
+  })
+
+  Phonebook.findByIdAndUpdate(request.params.id, entry, {new: true})
+  .then(updatedEntry=>{
+    response.json(updatedEntry)
+  })
+  .catch(error=>next(error))
 })
+
+app.delete('/api/persons/:id', (request, response, next)=>{
+  Phonebook.findByIdAndDelete(request.params.id)
+  .then(result=>{
+    response.status(204).end()
+  })
+  .catch(error=>next(error))
+})
+
+const errorHandler = (error, request, response, next)=>{
+  console.log(error.message);
+  if(error.name==='CastError'){
+    return response.status(400).send({error: 'malformatted id'})
+  }
+  next(error)
+}
+
+app.use(errorHandler)
 
 app.get('/api/persons/:id', (request, response)=>{
   const id = Number(request.params.id);
